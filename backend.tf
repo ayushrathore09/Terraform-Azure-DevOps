1b4a52718ca04d7ef5caa6eb3a2dc7370a51e561
ayush <ayushrathore0909@gmail.com>
2025-11-11 15:47:11 +0530

    add

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
     resource_group_name = "rg1"                 
-    storage_account_name = "ayushbackendtfstate"                              
+    storage_account_name = "remotestatefile001"                              
     container_name       = "tfstate"                               
-    key                  = "prod.terraform.tfstate"          
+    key                  = "dev.terraform.tfstate"          
   }
 }
